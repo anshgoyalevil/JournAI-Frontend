e68e5ec94482500d82911fd178fae0b007747466
Ansh Goyal <anshgoyal1704@gmail.com>
2024-08-22 18:17:30 +0530

    nit: remove redundant env variable

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,9 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    define: {
-      'process.env.VITE_APP_PLACES_API_KEY': JSON.stringify(env.VITE_APP_PLACES_API_KEY)
-    },
     plugins: [react(), tsconfigPaths()],
     test: {
       globals: true,
