8558fc385570cc4c18f0c6bf389d1c8d8860561f
Ansh Goyal <anshgoyal1704@gmail.com>
2024-08-22 17:51:11 +0530

    feat: ad lottie loader

--- src/pages/NewTripPage/NewTripPage.tsx
+++ src/pages/NewTripPage/NewTripPage.tsx
@@ -11,6 +11,7 @@ import {
   Chip,
   Group,
   NumberInput,
+  useComputedColorScheme,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import {
@@ -24,12 +25,27 @@ import { DatePickerInput } from '@mantine/dates';
 import { useState } from 'react';
 import axios from 'axios';
 import { notifications } from '@mantine/notifications';
+import Lottie from 'react-lottie';
+import AnimatedLoader from '../../lotties/animateLoader.json';
+import AnimatedLoaderDark from '../../lotties/animateLoaderDark.json';
 import classes from './NewTripPage.module.css';
 
 export default function NewTripPage() {
   const [prefs, setPrefs] = useState(['Museums', 'Historical']);
   const [budget, setBudget] = useState('');
   const [people, setPeople] = useState<string | number>(1);
+  const [loading, setLoading] = useState(false);
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
+  const defaultOptionsForLottie = {
+    loop: true,
+    autoplay: true,
+    animationData: computedColorScheme === 'dark' ? AnimatedLoaderDark : AnimatedLoader,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [response, setResponse] = useState<any>(null);
   const form = useForm({
@@ -70,6 +86,7 @@ export default function NewTripPage() {
       uniqId,
     };
     try {
+      setLoading(true);
       const res = await axios.post('http://127.0.0.1:8080/api/newtrip', formData);
 
       if (res.status === 201) {
@@ -91,7 +108,10 @@ export default function NewTripPage() {
       } else if (res.status === 201) {
         localStorage.setItem('requests', JSON.stringify([formData]));
       }
+      setLoading(false);
+      window.location.href = `/trip/${uniqId}`;
     } catch (error) {
+      setLoading(false);
       notifications.show({
         title: 'Error submitting form',
         message: 'Please try again later',
@@ -100,120 +120,132 @@ export default function NewTripPage() {
   };
 
   return (
-    <Container className={classes.wrapper} size={1400}>
-      <Title ta="center" mt={-30} order={2}>
-        Plan your next adventure
-      </Title>
-      <Center>
-        <Box mt={40} w={500}>
-          <form onSubmit={form.onSubmit(submitNewTrip)}>
-            <Text fw={600}>Where do you want to go?</Text>
-            {form.values.placesDates.map((placeDate: any, index) => (
-              <Box key={index} mt={index === 0 ? 15 : 0} display="flex">
-                <Box flex={1}>
-                  <TextInput
-                    placeholder="Enter a city or country"
-                    value={placeDate.place}
-                    onChange={(event) =>
-                      form.setFieldValue(`placesDates.${index}.place`, event.currentTarget.value)
-                    }
-                    required
-                    leftSection={<IconLocation style={{ width: rem(18), height: rem(18) }} />}
-                  />
-                  <DatePickerInput
-                    mt={10}
-                    type="range"
-                    placeholder="Select dates"
-                    value={placeDate.date}
-                    onChange={(dates) => form.setFieldValue(`placesDates.${index}.date`, dates)}
-                    clearable
-                    required
-                    leftSection={<IconCalendar style={{ width: rem(18), height: rem(18) }} />}
-                  />
-                  {index !== form.values.placesDates.length - 1 && (
-                    <Divider ml={30} h={30} size="sm" orientation="vertical" />
-                  )}
-                </Box>
-                {index > 0 && (
-                  <Button
-                    mt={0}
-                    ml={10}
-                    color="red"
-                    variant="light"
-                    onClick={() => deletePlaceDate(index)}
-                  >
-                    <IconTrash size={16} />
+    <>
+      {loading && (
+        <Center h={600}>
+          <Lottie options={defaultOptionsForLottie} height={400} width={400} />
+        </Center>
+      )}
+      {!loading && (
+        <Container className={classes.wrapper} size={1400}>
+          <Title ta="center" mt={-30} order={2}>
+            Plan your next adventure
+          </Title>
+          <Center>
+            <Box mt={40} w={500}>
+              <form onSubmit={form.onSubmit(submitNewTrip)}>
+                <Text fw={600}>Where do you want to go?</Text>
+                {form.values.placesDates.map((placeDate: any, index) => (
+                  <Box key={index} mt={index === 0 ? 15 : 0} display="flex">
+                    <Box flex={1}>
+                      <TextInput
+                        placeholder="Enter a city or country"
+                        value={placeDate.place}
+                        onChange={(event) =>
+                          form.setFieldValue(
+                            `placesDates.${index}.place`,
+                            event.currentTarget.value
+                          )
+                        }
+                        required
+                        leftSection={<IconLocation style={{ width: rem(18), height: rem(18) }} />}
+                      />
+                      <DatePickerInput
+                        mt={10}
+                        type="range"
+                        placeholder="Select dates"
+                        value={placeDate.date}
+                        onChange={(dates) => form.setFieldValue(`placesDates.${index}.date`, dates)}
+                        clearable
+                        required
+                        leftSection={<IconCalendar style={{ width: rem(18), height: rem(18) }} />}
+                      />
+                      {index !== form.values.placesDates.length - 1 && (
+                        <Divider ml={30} h={30} size="sm" orientation="vertical" />
+                      )}
+                    </Box>
+                    {index > 0 && (
+                      <Button
+                        mt={0}
+                        ml={10}
+                        color="red"
+                        variant="light"
+                        onClick={() => deletePlaceDate(index)}
+                      >
+                        <IconTrash size={16} />
+                      </Button>
+                    )}
+                  </Box>
+                ))}
+                <Button size="compact-sm" variant="light" mt={20} onClick={addPlaceDate}>
+                  + Add destination
+                </Button>
+                <Text fw={600} mt={30}>
+                  Select the kind of activities you want to do
+                </Text>
+                <Chip.Group multiple value={prefs} onChange={setPrefs}>
+                  <Group justify="center" mt="md">
+                    <Chip variant="outline" value="Kid Friendly">
+                      🛝 Kid Friendly
+                    </Chip>
+                    <Chip variant="outline" value="Museums">
+                      🖼️ Museums
+                    </Chip>
+                    <Chip variant="outline" value="Outdoor Adventures">
+                      🎴 Outdoor Adventures
+                    </Chip>
+                    <Chip variant="outline" value="Shopping">
+                      🛒 Shopping
+                    </Chip>
+                    <Chip variant="outline" value="Historical">
+                      🧱 Historical
+                    </Chip>
+                    <Chip variant="outline" value="Art & Cultural">
+                      🎨 Art & Cultural
+                    </Chip>
+                    <Chip variant="outline" value="Amusement Parks">
+                      🎡 Amusement Parks
+                    </Chip>
+                    <Chip variant="outline" value="Religious">
+                      ⛪ Religious
+                    </Chip>
+                    <Chip variant="outline" value="Pet Friendly">
+                      🐱 Pet Friendly
+                    </Chip>
+                  </Group>
+                </Chip.Group>
+                <Text fw={600} mt={30}>
+                  What is your expected budget?
+                </Text>
+                <TextInput
+                  mt={15}
+                  placeholder="$200"
+                  value={budget}
+                  onChange={(event) => setBudget(event.currentTarget.value)}
+                  required
+                  leftSection={<IconMoneybag style={{ width: rem(18), height: rem(18) }} />}
+                />
+                <Text fw={600} mt={30}>
+                  How many people are going?
+                </Text>
+                <NumberInput
+                  mt={15}
+                  placeholder="2"
+                  value={people}
+                  onChange={setPeople}
+                  required
+                  leftSection={<IconUsersGroup style={{ width: rem(18), height: rem(18) }} />}
+                />
+                <Center>
+                  <Button justify="center" variant="light" mt={40} type="submit">
+                    Create New Trip
                   </Button>
-                )}
-              </Box>
-            ))}
-            <Button size="compact-sm" variant="light" mt={20} onClick={addPlaceDate}>
-              + Add destination
-            </Button>
-            <Text fw={600} mt={30}>
-              Select the kind of activities you want to do
-            </Text>
-            <Chip.Group multiple value={prefs} onChange={setPrefs}>
-              <Group justify="center" mt="md">
-                <Chip variant="outline" value="Kid Friendly">
-                  🛝 Kid Friendly
-                </Chip>
-                <Chip variant="outline" value="Museums">
-                  🖼️ Museums
-                </Chip>
-                <Chip variant="outline" value="Outdoor Adventures">
-                  🎴 Outdoor Adventures
-                </Chip>
-                <Chip variant="outline" value="Shopping">
-                  🛒 Shopping
-                </Chip>
-                <Chip variant="outline" value="Historical">
-                  🧱 Historical
-                </Chip>
-                <Chip variant="outline" value="Art & Cultural">
-                  🎨 Art & Cultural
-                </Chip>
-                <Chip variant="outline" value="Amusement Parks">
-                  🎡 Amusement Parks
-                </Chip>
-                <Chip variant="outline" value="Religious">
-                  ⛪ Religious
-                </Chip>
-                <Chip variant="outline" value="Pet Friendly">
-                  🐱 Pet Friendly
-                </Chip>
-              </Group>
-            </Chip.Group>
-            <Text fw={600} mt={30}>
-              What is your expected budget?
-            </Text>
-            <TextInput
-              mt={15}
-              placeholder="$200"
-              value={budget}
-              onChange={(event) => setBudget(event.currentTarget.value)}
-              required
-              leftSection={<IconMoneybag style={{ width: rem(18), height: rem(18) }} />}
-            />
-            <Text fw={600} mt={30}>
-              How many people are going?
-            </Text>
-            <NumberInput
-              mt={15}
-              placeholder="2"
-              value={people}
-              onChange={setPeople}
-              required
-              leftSection={<IconUsersGroup style={{ width: rem(18), height: rem(18) }} />}
-            />
-            <Center>
-              <Button justify="center" variant="light" mt={40} type="submit">
-                Create New Trip
-              </Button>
-            </Center>
-          </form>
-        </Box>
-      </Center>
-    </Container>
+                </Center>
+              </form>
+            </Box>
+          </Center>
+        </Container>
+      )}
+    </>
   );
 }
