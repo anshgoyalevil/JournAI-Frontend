c376ae952d992f16d9b4d759de4c5da16ca3c701
Ansh Goyal <anshgoyal1704@gmail.com>
2024-08-22 16:57:52 +0530

    fix: improve styling of trip page

--- src/pages/TripPage/TripPage.tsx
+++ src/pages/TripPage/TripPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Timeline, Text, Title, Center } from '@mantine/core';
+import { Container, Timeline, Text, Title, Center, Grid } from '@mantine/core';
 import { IconMapPin, IconClock, IconCurrencyDollar } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -37,42 +37,48 @@ export default function TripPage() {
       <Title ta="center" mt={-30} mb={40} order={2}>
         Your trip itinerary
       </Title>
-      {Object.keys(activitiesByDestination).map((destination: any) => (
-        <div key={destination}>
-          <Title mb={20} mt={20} order={3}>
-            Destination: {destination}
-          </Title>
-          {Object.keys(activitiesByDestination[destination]).map((day: any) => (
-            <div key={day}>
-              <Title mb={10} mt={10} order={4}>
-                Day {day}
+      <Grid>
+        {Object.keys(activitiesByDestination).map((destination: any) => (
+          <Grid.Col px={30} span={{ base: 12, xs: 6 }}>
+            <div key={destination}>
+              <Title mb={20} mt={20} order={3}>
+                Destination: {destination}
               </Title>
-              <Timeline active={100} bulletSize={32} radius="md" lineWidth={2}>
-                {activitiesByDestination[destination][day].map((activity: any, index: number) => (
-                  <Timeline.Item
-                    key={index}
-                    bullet={<IconMapPin size={20} />}
-                    title={activity.name}
-                  >
-                    <Text size="xs" mt={4}>
-                      <Center inline>
-                        <IconClock size={14} style={{ marginRight: 5 }} />
-                        Duration: {activity.duration} hours
-                      </Center>
-                    </Text>
-                    <Text size="xs" mt={4}>
-                      <Center inline>
-                        <IconCurrencyDollar size={14} style={{ marginRight: 5 }} />
-                        Cost: ${activity.cost}
-                      </Center>
-                    </Text>
-                  </Timeline.Item>
-                ))}
-              </Timeline>
+              {Object.keys(activitiesByDestination[destination]).map((day: any) => (
+                <div key={day}>
+                  <Title mb={10} mt={10} order={4}>
+                    Day {day}
+                  </Title>
+                  <Timeline active={100} bulletSize={32} radius="md" lineWidth={2}>
+                    {activitiesByDestination[destination][day].map(
+                      (activity: any, index: number) => (
+                        <Timeline.Item
+                          key={index}
+                          bullet={<IconMapPin size={20} />}
+                          title={activity.name}
+                        >
+                          <Text size="xs" mt={4}>
+                            <Center inline>
+                              <IconClock size={14} style={{ marginRight: 5 }} />
+                              Duration: {activity.duration} hours
+                            </Center>
+                          </Text>
+                          <Text size="xs" mt={4}>
+                            <Center inline>
+                              <IconCurrencyDollar size={14} style={{ marginRight: 5 }} />
+                              Cost: ${activity.cost}
+                            </Center>
+                          </Text>
+                        </Timeline.Item>
+                      )
+                    )}
+                  </Timeline>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ))}
+          </Grid.Col>
+        ))}
+      </Grid>
     </Container>
   );
 }
